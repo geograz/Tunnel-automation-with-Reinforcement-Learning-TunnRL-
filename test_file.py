971274d24fb00ba7ac7c5869eb3ab3af4a1a7f46
G. H. Erharter <32653899+geograz@users.noreply.github.com>
2020-09-29 21:35:19 +0200

    Add files via upload
    test file for the repository

---
+++ test_file.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+test code for Github repo
+"""
+
+
+epsilon = 1  # initial exploration
+MIN_EPSILON = 0.05  # final exploration
+EPSILON_DECAY = 0.99997  # Every episode will be epsilon*EPS_DECAY
+
+step = 1
+
+while epsilon > MIN_EPSILON:
+    epsilon *= EPSILON_DECAY
+    step += 1
+
+print(f'it wil take {step} episodes to reach the minimum epsilon')
+
